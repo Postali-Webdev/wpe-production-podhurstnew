b4158226849eb62c453155efedfb7e01e8d544d0
Postali Web Development <181128403+Postali-Webdev@users.noreply.github.com>
2025-12-17 15:39:54 -0500

    gtm delay - test2

--- postali-crest/header.php
+++ postali-crest/header.php
@@ -19,20 +19,32 @@
 	<!-- End Google Tag Manager -->
 
 	<script>
-		var loadGtm = function(w, d, s, l, i) {
-		w[l] = w[l] || [];
-		w[l].push({
-			'gtm.start': new Date().getTime(),
-			event: 'gtm.js'
-		});
-		var f = d.getElementsByTagName(s)[0],
-			j = d.createElement(s),
-			dl = l != 'dataLayer' ? '&l=' + l : '';
-		j.async = true;
-		j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-		f.parentNode.insertBefore(j, f);
+		document.addEventListener('DOMContentLoaded', () => {
+			/** init gtm after 3500 seconds - this could be adjusted */
+			setTimeout(initGTM, 500);
+			});
+			document.addEventListener('scroll', initGTMOnEvent);
+			document.addEventListener('mousemove', initGTMOnEvent);
+			document.addEventListener('touchstart', initGTMOnEvent);
+			function initGTMOnEvent(event) {
+			initGTM();
+			event.currentTarget.removeEventListener(event.type, initGTMOnEvent); // remove the event listener that got triggered
+			}
+			function initGTM() {
+			if (window.gtmDidInit) {
+				return false;
+			}
+			window.gtmDidInit = true; // flag to ensure script does not get added to DOM more than once.
+			const script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.async = true;
+			// ensure PageViews is always tracked (on script load)
+			script.onload = () => {
+				dataLayer.push({ event: 'gtm.js', 'gtm.start': new Date().getTime(), 'gtm.uniqueEventId': 0 });
+			};
+			script.src = 'https://www.googletagmanager.com/gtm.js?id=YOUR-GTM-ID-HERE';
+			document.head.appendChild(script);
 		}
-		setTimeout(loadGtm.bind(null, window, document, 'script', 'dataLayer', 'GTM-N34NJN'), 500);
 	</script>
 
 
