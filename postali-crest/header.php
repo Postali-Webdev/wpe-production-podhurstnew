384a955492c134edaa64621e283cc2f72c75a851
Postali Web Development <181128403+Postali-Webdev@users.noreply.github.com>
2025-12-17 15:36:33 -0500

    testing gtm delay

--- postali-crest/header.php
+++ postali-crest/header.php
@@ -11,12 +11,31 @@
 	<link rel="preload" as="font" href="/wp-content/themes/postali-crest/assets/fonts/icomoon/icomoon.woff2"/>
 	
 	<!-- Google Tag Manager -->
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	<!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-N34NJN');</script>
+	})(window,document,'script','dataLayer','GTM-N34NJN');</script> -->
 	<!-- End Google Tag Manager -->
+
+	<script>
+		var loadGtm = function(w, d, s, l, i) {
+		w[l] = w[l] || [];
+		w[l].push({
+			'gtm.start': new Date().getTime(),
+			event: 'gtm.js'
+		});
+		var f = d.getElementsByTagName(s)[0],
+			j = d.createElement(s),
+			dl = l != 'dataLayer' ? '&l=' + l : '';
+		j.async = true;
+		j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+		f.parentNode.insertBefore(j, f);
+		}
+		setTimeout(loadGtm.bind(null, window, document, 'script', 'dataLayer', 'GTM-N34NJN'), 500);
+	</script>
+
+
 
 	<!-- Add JSON Schema here -->
     <?php 
