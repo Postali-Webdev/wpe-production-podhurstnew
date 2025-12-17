627750a70236b320a333cb4993fe3bfe9038db56
Postali Web Development <181128403+Postali-Webdev@users.noreply.github.com>
2025-12-17 16:13:46 -0500

    Update header.php

--- postali-crest/header.php
+++ postali-crest/header.php
@@ -9,14 +9,11 @@
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
 	<link rel="preload" as="font" href="/wp-content/themes/postali-crest/assets/fonts/icomoon/icomoon.woff2"/>
-	<link href="https://www.googletagmanager.com/gtag/js?id=GTM-N34NJN" rel="preload" as="script">
-	<link rel="dns-prefetch" href="https://www.googletagmanager.com/">
-
-
+	
 	<!-- Google Tag Manager -->
 	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;j.src=
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 	})(window,document,'script','dataLayer','GTM-N34NJN');</script>
 	<!-- End Google Tag Manager -->
